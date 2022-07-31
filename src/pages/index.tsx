c4a8bbaaca4825b9c1a03fd4e0cad182ee4b6f53
je-boska <joneirikboska@gmail.com>
2022-07-31 14:33:51 +0200

    Add page transitions

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,19 +2,35 @@ import type { InferGetStaticPropsType } from 'next';
 import { getProjects } from '../queries/projects';
 import Link from 'next/link';
 import { ProjectType } from '../types/shared';
+import Layout from '../components/Layout';
+import { motion } from 'framer-motion';
 
 const Home = ({ projects }: InferGetStaticPropsType<typeof getStaticProps>) => {
+  const variants = {
+    hidden: { opacity: 1, x: '-100%', y: 0 },
+    enter: { opacity: 1, x: 0, y: 0 },
+    exit: { opacity: 1, x: '-100%', y: 0 },
+  };
+
   return (
-    <div className='p-8'>
-      {projects.map(({ _id, slug, title, videoUrl, poster }) => (
-        <div key={_id}>
-          <Link href={`/projects/${slug}`}>
-            <h1 className='cursor-pointer'>{title}</h1>
-          </Link>
-          <video src={videoUrl} poster={poster} />
-        </div>
-      ))}
-    </div>
+    <Layout>
+      <motion.div
+        variants={variants} // Pass the variant object into Framer Motion
+        initial='hidden' // Set the initial state to variants.hidden
+        animate='enter' // Animated state to variants.enter
+        exit='exit' // Exit state (used later) to variants.exit
+        transition={{ type: 'linear' }} // Set the transition to linear
+      >
+        {projects.map(({ _id, slug, title, videoUrl, poster }) => (
+          <div key={_id}>
+            <Link href={`/projects/${slug}`} scroll={false}>
+              <h1 className='cursor-pointer'>{title}</h1>
+            </Link>
+            <video src={videoUrl} poster={poster} />
+          </div>
+        ))}
+      </motion.div>
+    </Layout>
   );
 };
 
--- src/pages/projects/[slug].tsx
+++ src/pages/projects/[slug].tsx
@@ -3,16 +3,37 @@ import React from 'react';
 import { getProjectPaths, getSingleProject } from '../../queries/projects';
 import BlockContent from '@sanity/block-content-to-react';
 import { ProjectType } from '../../types/shared';
+import Layout from '../../components/Layout';
+import Link from 'next/link';
+import { motion } from 'framer-motion';
 
 export default function Project({
   project: { title, body, videoUrl, poster },
 }: InferGetStaticPropsType<typeof getStaticProps>) {
+  const variants = {
+    hidden: { opacity: 1, x: '100%', y: 0 },
+    enter: { opacity: 1, x: 0, y: 0 },
+    exit: { opacity: 1, x: '100%', y: 0 },
+  };
+
   return (
-    <div className='p-8'>
-      <h1>{title}</h1>
-      {body ? <BlockContent blocks={body} /> : null}
-      <video src={videoUrl} poster={poster} />
-    </div>
+    <Layout>
+      <motion.div
+        variants={variants} // Pass the variant object into Framer Motion
+        initial='hidden' // Set the initial state to variants.hidden
+        animate='enter' // Animated state to variants.enter
+        exit='exit' // Exit state (used later) to variants.exit
+        transition={{ type: 'linear' }} // Set the transition to linear
+        className='p-8'
+      >
+        <Link href='/' scroll={false}>
+          <h1 className='cursor-pointer'>Home</h1>
+        </Link>
+        <h1>{title}</h1>
+        {body ? <BlockContent blocks={body} /> : null}
+        <video src={videoUrl} poster={poster} />
+      </motion.div>
+    </Layout>
   );
 }
 

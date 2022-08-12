285c8f417d7a303fab158481f17118d58b628a1c
je-boska <joneirikboska@gmail.com>
2022-08-12 21:35:43 +0200

    Set heading line height

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -7,7 +7,7 @@ export default function Hero() {
       <p className='absolute left-4 top-4 text-2xl'>?</p>
       <p className='absolute right-4 top-4 text-2xl'>P</p>
       <HeroFilter />
-      <h1 className='hero-heading absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 text-center font-title text-3xl md:text-5xl xl:text-8xl'>
+      <h1 className='hero-heading absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 text-center font-title text-3xl md:text-5xl xl:text-8xl leading-tight md:leading-tight xl:leading-tight'>
         Studio Conflux
       </h1>
     </div>
